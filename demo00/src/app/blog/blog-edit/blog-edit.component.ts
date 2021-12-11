705a88a3a374883a6bff9640554f7b0a991dc9ba
dhruvillsangani <dhruvil.s@prominentpixel.com>
2021-12-11 09:47:32 +0530

    l1

--- demo00/src/app/blog/blog-edit/blog-edit.component.ts
+++ demo00/src/app/blog/blog-edit/blog-edit.component.ts
@@ -10,14 +10,33 @@ import { BlogService } from 'src/app/blog.service';
   styleUrls: ['./blog-edit.component.css']
 })
 export class BlogEditComponent implements OnInit {
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
+  imageurl:string;
 
   id: number;
   blogarr = {id:0,title:"title1",imageurl:"https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",description:"description1",author:"author1",tags:[{item_id: 1, item_text: 'java'}],date:"12-12-12"};
 
-  constructor(private modalService: NgbModal,private LoginService:LoginService,public BlogService:BlogService,private router:Router,
+  constructor(private modalService: NgbModal,public LoginService:LoginService,public BlogService:BlogService,private router:Router,
     private route:ActivatedRoute) { }
 
   ngOnInit(){
+    this.dropdownList = [
+      { item_id: 1, item_text: 'java' },
+      { item_id: 2, item_text: 'Javascript' },
+      { item_id: 3, item_text: 'C++' },
+    ];
+    this.dropdownSettings= {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
@@ -26,6 +45,20 @@ export class BlogEditComponent implements OnInit {
       }
     );
   }
+
+  onsubmit(details) {
+
+    let date = new Date().toString();
+    let title = details.title;
+    let description = details.description;
+    let tags= details.tags;
+    let imageurl = details.imageurl;
+    let author = details.author;
+    let id = this.BlogService.getBlogLength()+1;
+     this.BlogService.addData(id,title,imageurl,description,author,tags,date)
+     this.router.navigate(['../'],{relativeTo: this.route});
+    
+   }
 
 }
 

27d936e9d44ea25b4cf877310e6f7fde34d11235
dhruvillsangani <dhruvil.s@prominentpixel.com>
2021-12-20 11:56:02 +0530

    last1

--- 18.Making Http Requests/Http/src/app/emp.service.ts
+++ 18.Making Http Requests/Http/src/app/emp.service.ts
@@ -3,26 +3,18 @@ import { Injectable, OnInit } from "@angular/core";
 @Injectable({providedIn: 'root'}) 
 
 export class empService implements OnInit{
- 
+     
+    arr = [{username:"Dhruvil",
+           company:'prominentpixel',
+           area:'rajkot',
+           gender:'male',
+           newtocompany:'yes',
+           mno:'919999999'}];
        
-
-      
-    arr = [{username:"Dhruvil",company:'prominentpixel',area:'rajkot',gender:'male',newtocompany:'yes',mno:'919999999'}];
-       
-    ngOnInit() {
-           
-        }
+    ngOnInit() {}
 
         addEmp(details) {
-
             this.arr.push(details);
             console.log(details);
-            
-
-
-
         }
- 
-
-    
 }
